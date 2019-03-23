021899f12e5030a8976ad5c91ab2e626d9f5224d
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-03-23 18:59:04 +0100

    Add window creation

--- src/Clippic/SelectionForm.cs
+++ src/Clippic/SelectionForm.cs
@@ -1,8 +1,24 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace Clippic
 {
+    /// <summary>
+    /// Form for making the selection.
+    /// </summary>
+    /// <seealso cref="Form" />
     public class SelectionForm : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SelectionForm"/> class.
+        /// </summary>
+        public SelectionForm()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            ShowInTaskbar = false;
+            BackColor = Color.Red; // Does not work for all colours. Thank you Microsoft.
+            TransparencyKey = BackColor;
+        }
     }
 }
