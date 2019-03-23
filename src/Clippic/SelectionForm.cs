e0a90d2bf6180a23f160b9f99575ef58b4ae80fa
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-03-23 19:38:11 +0100

    Implement screenshotting

--- src/Clippic/SelectionForm.cs
+++ src/Clippic/SelectionForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Clippic
@@ -9,16 +10,77 @@ namespace Clippic
     /// <seealso cref="Form" />
     public class SelectionForm : Form
     {
+        private Point selectionStart;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="SelectionForm"/> class.
         /// </summary>
         public SelectionForm()
         {
             FormBorderStyle = FormBorderStyle.None;
-            WindowState = FormWindowState.Maximized;
             ShowInTaskbar = false;
             BackColor = Color.Red; // Does not work for all colours. Thank you Microsoft.
             TransparencyKey = BackColor;
+
+            MouseDown += StartSelection;
+            MouseUp += StopSelection;
+            Load += WindowLoaded;
         }
+
+        /// <summary>
+        /// Event fired when the window is loaded.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void WindowLoaded(object sender, EventArgs e)
+        {
+            Size = SystemInformation.VirtualScreen.Size;
+            Location = SystemInformation.VirtualScreen.Location;
+        }
+
+        /// <summary>
+        /// Event fired when the selection starts.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
+        private void StartSelection(object sender, MouseEventArgs e)
+        {
+            selectionStart = GetGlobalLocation(e.Location);
+        }
+
+        /// <summary>
+        /// Event fired when the selection ends.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
+        private void StopSelection(object sender, MouseEventArgs e)
+        {
+            if (selectionStart != null)
+            {
+                Point selectionEnd = GetGlobalLocation(e.Location);
+                int xMin = Math.Min(selectionStart.X, selectionEnd.X);
+                int xMax = Math.Max(selectionStart.X, selectionEnd.X);
+                int yMin = Math.Min(selectionStart.Y, selectionEnd.Y);
+                int yMax = Math.Max(selectionStart.Y, selectionEnd.Y);
+
+                Bitmap bmp = new Bitmap(xMax - xMin, yMax - yMin);
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.CopyFromScreen(xMin, yMin, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+                }
+
+                Clipboard.SetImage(bmp);
+            }
+
+            Close();
+        }
+
+        /// <summary>
+        /// Gets the global location of a point in the window.
+        /// </summary>
+        /// <param name="point">The point in the window.</param>
+        /// <returns>The global location of the point in the window.</returns>
+        private Point GetGlobalLocation(Point point)
+            => new Point(point.X + Location.X, point.Y + Location.Y);
     }
 }
