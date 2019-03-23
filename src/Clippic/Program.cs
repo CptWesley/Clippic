61522614c6fabc64de8f0d04db056a30e27fe6a2
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-03-23 18:39:24 +0100

    Add basic project

---
+++ src/Clippic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Clippic
+{
+    /// <summary>
+    /// Entry point of the program.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SelectionForm());
+        }
+    }
+}
---
+++ src/Clippic/SelectionForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows.Forms;
+
+namespace Clippic
+{
+    /// <summary>
+    /// Form used for making the screenshot selection.
+    /// </summary>
+    /// <seealso cref="Form" />
+    public partial class SelectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+
+        #endregion
+    }
+}
+
