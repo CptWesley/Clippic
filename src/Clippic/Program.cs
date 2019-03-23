cd89b16b9a51db74238fec1d77c2ba4621444c8d
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-03-23 18:48:42 +0100

    Add Less poluted SelectionForm

--- src/Clippic/Program.cs
+++ src/Clippic/Program.cs
@@ -16,7 +16,7 @@ namespace Clippic
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form());
+            Application.Run(new SelectionForm());
         }
     }
 }
---
+++ src/Clippic/SelectionForm.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Forms;
+
+namespace Clippic
+{
+    public class SelectionForm : Form
+    {
+    }
+}
